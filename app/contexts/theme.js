7eb9a48206fa487e55d5bd8922f45a5849412acc
Mark Njoroge <marknjo7@gmail.com>
2020-08-08 14:08:17 +0300

    Refactored Player input to use React Hooks

--- app/contexts/theme.js
+++ app/contexts/theme.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const { Consumer, Provider } = React.createContext()
+const Context = React.createContext();
+const { Consumer, Provider } = Context;
 
-export const ThemeConsumer = Consumer
-export const ThemeProvider = Provider
+/* export const ThemeConsumer = Consumer;
+export const ThemeProvider = Provider; */
+
+export {
+	Consumer as ThemeConsumer,
+	Provider as ThemeProvider,
+	Context as default,
+};
